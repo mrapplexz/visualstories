2174ee64d544bd3cdd5a79fabb393d781db7c9bf
mrAppleXZ <mr.applexz@gmail.com>
2021-09-18 21:43:52 +0500

    Try to fix git submodule initialization

--- install.sh
+++ install.sh
@@ -2,8 +2,7 @@
 cd "$(dirname "$0")"
 
 pip install -r requirements.txt
-git submodule init --recursive
-cd diffvg
 git submodule update --init --recursive
+cd diffvg
 python setup.py install
 cd ..
